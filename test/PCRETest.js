5e75e50fff464718ccf0ae07f518acab70f62e4b
Eric McCarthy <eric@limulus.net>
2018-07-01 23:21:22 -0700

    Test .load() independently, mainly becase it is slow

--- test/PCRETest.js
+++ test/PCRETest.js
@@ -3,6 +3,13 @@ import assert from 'assert'
 import PCRE from '../src/lib/PCRE'
 
 describe(`PCRE`, function () {
+  describe(`load()`, function () {
+    it(`should not throw`, async function () {
+      this.timeout(5000)
+      await PCRE.load()
+    })
+  })
+
   describe(`version()`, function () {
     it(`should return the version of the PCRE2 library`, async function () {
       const version = await PCRE.version()
